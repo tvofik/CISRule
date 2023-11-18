aa9da5c9236674bfb5f3342eb994710b92c9488c
Abayomi Pelewura <17411689+tvofik@users.noreply.github.com>
2023-11-18 01:53:15 -0500

    added version

--- powershell_script.ps1
+++ powershell_script.ps1
@@ -1,27 +1,30 @@
 # working powershell script 
 $collection = '
-[
-  {
-    "Path": "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System:DontDisplayLastUserName",
-    "ExpectedValue": 0
-  },
-  {
-    "Path":"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System:EnableLUA",
-    "ExpectedValue": 1
-  },
-  {
-    "Path": "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System:EnableUwpStartupTasks",
-    "ExpectedValue": 2
-  },
-  {
-    "Path": "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System:EnableCursorSuppression",
-    "ExpectedValue": 1
-  },
-  {
-    "Path": "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System:ConsentPromptBehaviorAdmin",
-    "ExpectedValue": 5
-  }
-]
+{
+  "Version": "v2.0.0",
+  "Checks": [
+    {
+      "Path": "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System:DontDisplayLastUserName",
+      "ExpectedValue": 0
+    },
+    {
+      "Path":"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System:EnableLUA",
+      "ExpectedValue": 1
+    },
+    {
+      "Path": "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System:EnableUwpStartupTasks",
+      "ExpectedValue": 2
+    },
+    {
+      "Path": "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System:EnableCursorSuppression",
+      "ExpectedValue": 1
+    },
+    {
+      "Path": "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System:ConsentPromptBehaviorAdmin",
+      "ExpectedValue": 5
+    }
+  ]
+}
 '
 
 $baseRegistryPath = "HKLM:\"
@@ -30,9 +33,11 @@ $nonComplaint = 0
 
 $collectionObject = ConvertFrom-Json $collection
 
-foreach ($item in $collectionObject) {
-  $PathParts = ($item.Path).Split(":")
-  $expectedValue = $item.ExpectedValue
+$version = $collectionObject.Version
+
+foreach ($check in $collectionObject.Checks) {
+  $PathParts = ($check.Path).Split(":")
+  $expectedValue = $check.ExpectedValue
 
   $registryPath = $PathParts[0]
   $propertyName = $PathParts[-1]
@@ -49,4 +54,4 @@ foreach ($item in $collectionObject) {
   }
 }
 
-Write-Host "Completed CIS Checks`nRESULT: $compliant compliant and $nonComplaint non-compliant items"
+Write-Host "Completed CIS Checks $version`nRESULT: $compliant compliant and $nonComplaint non-compliant items"
