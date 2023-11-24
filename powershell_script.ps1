9cbed31b3f8fd951f91a63cd552440a2addad624
Abayomi Pelewura <17411689+tvofik@users.noreply.github.com>
2023-11-24 13:59:40 -0500

    updated the powershell script

--- powershell_script.ps1
+++ powershell_script.ps1
@@ -30,21 +30,24 @@ $collection = '
 $baseRegistryPath = "HKLM:\"
 $compliant = 0
 $nonComplaint = 0
+$totalChecks = 0
 
 $collectionObject = ConvertFrom-Json $collection
 
 $version = $collectionObject.Version
 
 foreach ($check in $collectionObject.Checks) {
+  $totalChecks++
+
   $PathParts = ($check.Path).Split(":")
   $expectedValue = $check.ExpectedValue
 
   $registryPath = $PathParts[0]
   $propertyName = $PathParts[-1]
 
-  $fullregistryPath = $baseRegistryPath + $registryPath.Trim()
+  $fullRegistryPath = $baseRegistryPath + $registryPath.Trim()
     
-  $result = (Get-ItemProperty -Path $fullregistryPath).$propertyName
+  $result = (Get-ItemProperty -Path $fullRegistryPath).$propertyName
 
   if ($result -eq $expectedValue) {
     $compliant++
@@ -54,4 +57,4 @@ foreach ($check in $collectionObject.Checks) {
   }
 }
 
-Write-Host "Completed CIS Checks $version`nRESULT: $compliant compliant and $nonComplaint non-compliant items"
+Write-Host "Completed CIS Checks $version`nRESULT: Total checks $totalChecks ( $compliant compliant and $nonComplaint non-compliant items )"
